3e40b7397e6e639043d02dc42ba008fdb69053bd
Maryanne Wachter <maryanne.wachter@gmail.com>
2021-08-22 11:42:26 -0700

    modification for trying smelte styles

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Button from "smelte/src/components/Button";
 	import { spring } from 'svelte/motion';
 
 	let count = 0;
@@ -14,24 +15,20 @@
 </script>
 
 <div class="counter">
-	<button on:click={() => (count -= 1)} aria-label="Decrease the counter by one">
-		<svg aria-hidden="true" viewBox="0 0 1 1">
-			<path d="M0,0.5 L1,0.5" />
-		</svg>
-	</button>
-
-	<div class="counter-viewport">
-		<div class="counter-digits" style="transform: translate(0, {100 * offset}%)">
-			<strong style="top: -100%" aria-hidden="true">{Math.floor($displayed_count + 1)}</strong>
-			<strong>{Math.floor($displayed_count)}</strong>
-		</div>
-	</div>
-
-	<button on:click={() => (count += 1)} aria-label="Increase the counter by one">
-		<svg aria-hidden="true" viewBox="0 0 1 1">
-			<path d="M0,0.5 L1,0.5 M0.5,0 L0.5,1" />
-		</svg>
-	</button>
+ <Button on:click={() => (count -=1)} outlined>-</Button>
+ <div class="counter-viewport">
+   <div class="counter-digits"
+        style="tranform: translate(0, {100 * offset}%)">
+        <strong style="top: -100%"
+                aria-hidden="true">
+          {Math.floor($displayed_count + 1)}
+        </strong>
+        <strong>
+          {Math.floor($displayed_count)}
+        </strong>
+   </div>
+ </div>
+ <Button on:click={() => (count += 1)} outlined>+</Button>
 </div>
 
 <style>
@@ -40,33 +37,6 @@
 		border-top: 1px solid rgba(0, 0, 0, 0.1);
 		border-bottom: 1px solid rgba(0, 0, 0, 0.1);
 		margin: 1rem 0;
-	}
-
-	.counter button {
-		width: 2em;
-		padding: 0;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		border: 0;
-		background-color: transparent;
-		color: var(--text-color);
-		font-size: 2rem;
-	}
-
-	.counter button:hover {
-		background-color: var(--secondary-color);
-	}
-
-	svg {
-		width: 25%;
-		height: 25%;
-	}
-
-	path {
-		vector-effect: non-scaling-stroke;
-		stroke-width: 2px;
-		stroke: var(--text-color);
 	}
 
 	.counter-viewport {
--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,120 +1,27 @@
 <script>
-	import { page } from '$app/stores';
-	import logo from './svelte-logo.svg';
+ import { page } from '$app/stores';
+ import Tabs from 'smelte/src/components/Tabs';
+ import AppBar from 'smelte/src/components/AppBar';
+ import Button from 'smelte/src/components/Button';
+ import { Spacer } from 'smelte/src/components/Util';
+ import IconButton from '../IconButton.svelte';
+
+ $: path = $page ? $page.path ? $page.path : '/' : '/';
 </script>
 
-<header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
-	</div>
-
-	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
-		<ul>
-			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
-			<li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
-			<li class:active={$page.path === '/todos'}><a sveltekit:prefetch href="/todos">Todos</a></li>
-		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
-	</nav>
-
-	<div class="corner">
-		<!-- TODO put something else here? github link? -->
-	</div>
-</header>
-
-<style>
-	header {
-		display: flex;
-		justify-content: space-between;
-	}
-
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
-
-	nav {
-		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
-	}
-
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--background);
-		background-size: contain;
-	}
-
-	li {
-		position: relative;
-		height: 100%;
-	}
-
-	li.active::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--accent-color);
-	}
-
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 1em;
-		color: var(--heading-color);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 10%;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
-
-	a:hover {
-		color: var(--accent-color);
-	}
-</style>
+<AppBar classes="fixed top-0 w-screen items-center flex-wrap flex left-0 z-30 p-0 elevation-0 bg-dark-500 opacity-98">
+  <div class="w-4"
+       style="height: 52px" />
+  <IconButton icon="menu"/>
+  <Spacer/>
+  <a href="/"
+     class="flex items-center">
+    <IconButton icon="video_call" />
+  </a>
+  <div style="width: 6px;"/>
+  <a href="/about" class="flex items-center">
+    <IconButton icon="account_circle" />
+  </a>
+  <div class="w-4" style="height:52px"/>
+</AppBar>
+<div style="height: 52px;" />

9296706c84cd7a9b19dda9e05df78aae8b2d3cc7
Maryanne Wachter <maryanne.wachter@gmail.com>
2021-08-23 11:02:33 -0700

    fixed missing icon

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,7 +1,6 @@
 <script>
 	import AppBar from 'smelte/src/components/AppBar';
   import { Spacer } from 'smelte/src/components/Util';
-	import IconButton from '$lib/IconButton.svelte';
   import TextButton from '$lib/TextButton.svelte';
 	import FaButton from '$lib/FaButton.svelte';
 	import { faGithub } from '@fortawesome/free-brands-svg-icons';
@@ -12,7 +11,6 @@
 	classes="fixed top-0 w-screen items-center flex-wrap flex left-0 z-30 p-0 elevation-0 bg-dark-500 opacity-98"
 >
 	<div class="w-4" style="height: 52px" />
-	<IconButton icon="menu" />
 	<Spacer />
 	<div style="width: 6px;" />
 	<a href="/about" class="flex items-center">
